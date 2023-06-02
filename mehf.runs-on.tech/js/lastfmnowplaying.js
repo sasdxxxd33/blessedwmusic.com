f8fc6d3b4edcea688841a59f21db403060577a2d
arda <135357947+sasdxxxd33@users.noreply.github.com>
2023-06-02 20:40:31 +0300

    Add files via upload

---
+++ mehf.runs-on.tech/js/lastfmnowplaying.js
@@ -0,0 +1,7 @@
+function yess(){$.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mehf&api_key=9d93c9ed1ec5d370bed6a768664de7c5&limit=1&format=json',function(data){if(jQuery.type(data.recenttracks.track)==='array'){var nowPlaying=data.recenttracks.track[0];}else{var nowPlaying=data.recenttracks.track;}
+window.playing=nowPlaying['name']
+window.artist=nowPlaying['artist']['#text']
+window.link=nowPlaying.url})}
+var gamer=function(){var api_key='9d93c9ed1ec5d370bed6a768664de7c5';var username='mehf';var base_url='https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+username+'&api_key='+api_key+'&limit=1&format=json';$.getJSON(base_url,function(data){if(jQuery.type(data.recenttracks.track)==='array'){var nowPlaying=data.recenttracks.track[0];}else{var nowPlaying=data.recenttracks.track;}
+if(nowPlaying['@attr']&&nowPlaying['@attr']['nowplaying']){var listening_text='Currently listening to ';var cover=data.recenttracks.track[0].image[3]['#text']}else{var listening_text='Last listened to ';var cover="https://i.pinimg.com/originals/79/2d/01/792d01df5154f95d6a5e480a3a627892.gif"}
+var currentTrack=nowPlaying['name']+' by '+nowPlaying['artist']['#text'];$('#now_playing').html('<a href= '+nowPlaying.url+'>'+listening_text+currentTrack+'</a>');})};gamer();setInterval(gamer,10*2000)
---
+++ mehf.runs-on.tech/js/script.js
@@ -0,0 +1,3 @@
+const music=["music/audio.mp3","music/audio2.mp3","music/audio3.mp3","music/audio4.mp3","music/audio5.mp3","music/audio6.mp3","music/audio7.mp3","music/audio8.mp3"]
+var audio=new Audio(music[Math.floor(Math.random()*music.length)]);audio.volume=0.05;audio.loop=true
+document.onclick=function(){audio.play();}
