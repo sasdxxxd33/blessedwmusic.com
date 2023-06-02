24e7b8463eea6e60c68c2d8591c8244f6903fa25
arda <135357947+sasdxxxd33@users.noreply.github.com>
2023-06-02 20:37:35 +0300

    Add files via upload

---
+++ mehf/mehf.runs-on.tech/js/portfolio.js
@@ -0,0 +1,8 @@
+$(window).on('keydown',function(event)
+{if(event.keyCode==123)
+{return false;}
+else if(event.ctrlKey&&event.shiftKey&&event.keyCode==73)
+{return false;}
+else if(event.ctrlKey&&event.keyCode==73)
+{return false;}});$(document).on("contextmenu",function(e)
+{e.preventDefault();});
